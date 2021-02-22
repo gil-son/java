5f11f445f9e2eb367c3f6b6d50d940c9f1d01fea
gil-son <gilszon@gmail.com>
2021-02-21 22:48:26 -0300

    Seminary

--- Association/as02/src/as02/Seminary.java
+++ Association/as02/src/as02/Seminary.java
@@ -2,7 +2,7 @@ package as02;
 
 public class Seminary {
 	private String title;
-	private Student[] students;
+	private Student[] students; // You can create an array from the Student class
 	private Teacher teacher;
 	private Local local;
 	
@@ -13,6 +13,53 @@ public class Seminary {
 	public Seminary(String title) {
 		this.title = title;
 	}
+	
+	
+	public void print() {
+		// Can create a validation to verify if is null, before call any method get
+		
+		
+		System.out.println(
+				"\n------------- Relatory of Seminarys -------------\n"+
+				"\nTitle: "+this.title+"\nTeacher: ");
+		
+		/*
+		if(this.getTeacher() == null)
+			System.out.println("No teacher register");
+			
+		else
+			System.out.print(this.teacher.getName());	
+		*/
+		
+		try {
+			System.out.print(this.teacher.getName());
+		}catch(Exception e) {
+			System.out.print("No teacher register: "+e);
+		}
+		
+		
+		
+		
+		System.out.println("\nLocal:");
+		
+		if(local == null)
+				System.out.print("No location register");
+		else
+			System.out.print("\n\tStreet: "+this.local.getStreet()+"\n\tDistrict: "+this.local.getDistrict()+"\n");
+	
+	
+		System.out.println("\nStudent(s):");
+		
+			for(Student student: students) {
+				if(student.getName() == null) {
+					System.out.println("\nNo student register\n");
+				}else {
+					System.out.print("\n\t• " + student.getName());
+				}
+			}
+		}
+	
+	
 	
 	
 	public String getTitle() {
@@ -27,7 +74,7 @@ public class Seminary {
 		return students;
 	}
 
-	public void setStudents(Student[] students) {
+	public void setStudents(Student[] students) {  // Receive an array of object
 		this.students = students;
 	}
 
