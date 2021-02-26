113e4b4554935086dd827904f722213fbfbb038d
gil-son <gilszon@gmail.com>
2021-02-26 19:28:58 -0300

    Book Store

---
+++ ArrayList/src/ex02/AppBookStoreArrayListStatic.java
@@ -0,0 +1,16 @@
+package ex02;
+import java.util.Scanner;
+
+public class AppBookStoreArrayListStatic {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Scanner inputString = new Scanner(System.in);
+	
+		
+		
+		
+		
+	}
+
+}
--- ArrayList/src/ex02/Collection.java
+++ ArrayList/src/ex02/Collection.java
@@ -19,20 +19,70 @@ public class Collection {
 		listBooks.add(bs);
 	}
 	
-	// show all data of list object
 	
+	// show all data of list object
 	static public String showList() {
 		String saida = "";
-		
+		int i = 1;
 		//            bs to each item in listBooks
 		for(BookStore bs : listBooks) {
-			saida += bs.print();
+			saida += "\n ===== LIVRO Nº "+ (i++) +" =====\n" + bs.print();
+			saida += bs.print() + "\n";
 		}
 		
 		
 		return saida;
 	}
 	
+	// Research how many books exist there by genre
+	static public int search(String genre) {
+		int amount = 0;
+		//            bs to each item in listBooks
+		for(BookStore bs : listBooks ) {
+			if(bs.getGenre().equalsIgnoreCase(genre)) {
+				amount++;
+			}
+		}
+		
+		return amount;
+	}
+	
+	// Research how many prices between value insert exist there (Overload)
+	
+	static public int search(double initialPrice, double finalPrice) {
+		int amount = 0 ;
+		
+		for(BookStore bs : listBooks) {
+			if(bs.getPrice() >= initialPrice && bs.getPrice() <= finalPrice ) {
+				amount++;
+			}
+		}
+		
+		return amount;
+	}
+	
+	// Remove object/book by title
+	
+	static public boolean remover(String title) {
+		for(BookStore bs : listBooks) {
+			if(bs.getTitle().equalsIgnoreCase(title)) {
+				listBooks.remove(bs);
+				return true;
+			}
+		}
+		return false;
+		
+	}
 	
 	
+	// Total price 
+	static double calculateTotalCollection() {
+		double total = 0;
+		for(BookStore bs : listBooks) {
+			total += bs.getPrice();
+		}
+		
+		return total;
+		
+	}
 }
