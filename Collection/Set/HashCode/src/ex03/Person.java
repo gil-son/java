a189730850f1eb706b85a1c1f351743f3175862a
gil-son <gilszon@gmail.com>
2021-04-17 19:40:11 -0300

    Ex03

--- Collection/Set/HashCode/src/ex03/Person.java
+++ Collection/Set/HashCode/src/ex03/Person.java
@@ -1,4 +1,8 @@
 package ex03;
+
+import java.util.HashSet;
+import java.util.Set;
+
 // https://www.youtube.com/watch?v=mtdUbmp7uec&t=555s
 public class Person {
 	private String firstName;
@@ -24,17 +28,24 @@ public class Person {
 	}
 	
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(Object obj) { // Generic object
 		if(obj == null) return false;
 		if(!(obj instanceof Person)) return false;
-		Person other = (Person) obj; 
+		Person other = (Person) obj;  // The Generic object receive cast(Person) and the object 'other' receive obj 
 		
-		//System.out.println(other.getFirstName());
-		//System.out.println(((Person) obj).getFirstName());
+		
+		// System.out.println(obj);
+		// System.out.println(other);
+		
+		// System.out.println(other.getFirstName());
+		// System.out.println(((Person) obj).getFirstName());
+		
 		
 		return	this.firstName.equalsIgnoreCase(other.getFirstName()) &&
 				this.lastName.equalsIgnoreCase(other.getLastName()) &&
 				this.code == other.getCode();
+			 
+				// this = p1    obj/other = parameter obj
 	}
 	
 	
@@ -45,14 +56,19 @@ public class Person {
 		Person p3 = new Person("Aaa","of A",1);
 		
 		System.out.printf("p1 == p2? %s%n",p1==p2); // Objects are distinct in space Heap
-		System.out.printf("p1.equals(p2) %s%n",p1.equals(p2));
+		System.out.printf("p1.equals(p2) %s%n",p1.equals(p2)); // p1/this and all atributes is equals p2 and your all atributes?
 		
 		
 		System.out.printf("p1 == p3? %s%n",p1==p3);
 		System.out.printf("p1.equals(p3) %s%n",p1.equals(p3));
 		
-		
-		
+
+		Set<Person> set = new HashSet<>();
+		set.add(p1);
+		set.add(p1);
+		set.add(p2);
+		set.add(p3);
+		System.out.printf("Set has %d elements%n", set.size());
 	}
 	
 	
