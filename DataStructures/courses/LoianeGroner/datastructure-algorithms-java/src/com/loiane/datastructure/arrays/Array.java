252f86865cd7ca13c6e0b177ba881afbedf618f5
gil-son <gilszon@gmail.com>
2021-05-12 09:17:39 -0300

    Arrays

---
+++ DataStructures/courses/LoianeGroner/datastructure-algorithms-java/src/com/loiane/datastructure/arrays/Array.java
@@ -0,0 +1,10 @@
+package com.loiane.datastructure.arrays;
+
+public class Array {
+	
+	private String [] elements;
+	
+	public Array(int capacity) {
+		this.elements = new String[capacity];
+	}
+}
---
+++ DataStructures/courses/LoianeGroner/datastructure-algorithms-java/src/com/loiane/datastructure/arrays/test/ArrayTest.java
@@ -0,0 +1,11 @@
+package com.loiane.datastructure.arrays.test;
+
+import com.loiane.datastructure.arrays.Array;
+
+public class ArrayTest {
+	public static void main(String[] args) {
+		Array array = new Array(5);
+		int i = 0;
+		i++;
+	}
+}
