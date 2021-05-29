66279743076de4ea85c91341633bf49324860b3c
gil-son <gilszon@gmail.com>
2021-05-29 13:47:10 -0300

    Java #02

--- DataStructures/courses/LoianeGroner/datastructure-algorithms-java/src/com/loiane/datastructure/arrays/test/ArrayLesson02.java
+++ DataStructures/courses/LoianeGroner/datastructure-algorithms-java/src/com/loiane/datastructure/arrays/test/ArrayLesson02.java
@@ -2,7 +2,7 @@ package com.loiane.datastructure.arrays.test;
 
 import com.loiane.datastructure.arrays.Array;
 
-public class ArrayTest {
+public class ArrayLesson02 {
 	public static void main(String[] args) {
 		Array array = new Array(5);
 		int i = 0;
