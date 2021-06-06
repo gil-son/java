11e681b94d1d27adeb34ded2881316aa6af6a522
gil-son <gilszon@gmail.com>
2021-06-06 14:52:48 -0300

    Node and List

---
+++ DataStructures/concepts/LinkedList/src/ex01/util/List.java
@@ -0,0 +1,16 @@
+package ex01.util;
+
+public class List {
+	
+	private Node head;
+	
+	public void add(Double value) {
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+		return super.toString();
+	}
+}
---
+++ DataStructures/concepts/LinkedList/src/ex01/util/Node.java
@@ -0,0 +1,25 @@
+package ex01.util;
+
+public class Node {
+	
+	private Double value;
+	private Node next;
+	
+	
+	public Double getValue() {
+		return value;
+	}
+	
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	public Node getNext() {
+		return next;
+	}
+	
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+}
