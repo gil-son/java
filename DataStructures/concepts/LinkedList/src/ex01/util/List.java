3e7cd6fc979348a4daccb5b45e248cc1e2b8db8b
gil-son <gilszon@gmail.com>
2021-06-07 16:52:24 -0300

    Add and show element(s)

--- DataStructures/concepts/LinkedList/src/ex01/util/List.java
+++ DataStructures/concepts/LinkedList/src/ex01/util/List.java
@@ -1,6 +1,8 @@
 package ex01.util;
 
 public class List {
+	// 
+	
 	
 	private Node head;
 	
@@ -9,51 +11,90 @@ public class List {
 			node.setValue(value);
 			node.setNext(head);
 			head = node;
+			/*           head
+			 * list-->| |null|--_
+			 * 
+			 * 
+			 * value = 5
+			 * head = null
+			 * node = n1
+			 * 
+			 * node.setValue | node.setNext | head
+			 *    value=5        head=null   node=n1                                       
+			 * 
+			 * 		   head = n1 = node
+			 * list--> |value=5|next head=null|--_
+			 *                   		     
+			 *
+			 * value = 3
+			 * head = n1
+			 * node = n2
+			 * 
+			 * node.setValue | node.setNext | head
+			 *   value             head       node 
+			 *                                                   
+			 *          head = n2 = node            node=n1
+			 * list-->|value=3|next head=n1|------>|5|null|--_
+			 *   
+			 *                    
+			 * value = 7
+			 * head = n2
+			 * node = n3                  		                        
+			 *
+			 *
+			 * node.setValue | node.setNext | head
+			 *     value             head     node 
+			 *                                                   
+			 *           head=n3=node         n2=node      n1=node
+			 * list--> |7|next head=n2|------>|3|n1|------>|5|null|--_
+			 *
+			 *
+			 */
 		
-		/* 
-		 * list-->| |null|--_
-		 * 
-		 * 
-		 * value = 5
-		 * head = null
-		 * node = n1
-		 * 
-		 * node | node.setValue | node.setNext | head
-		 *            value          head        node                                       
-		 * 
-		 * 		             			   head = n1
-		 * list--> | |next head|------->|5|null|--_
-		 *                   		     
-		 *
-		 * value = 3
-		 * head = n1
-		 * node = n2
-		 * 
-		 * node | node.setValue | node.setNext | head
-		 *            value             head     node 
-		 *                                                   
-		 *                    		    n2           n1
-		 * list--> | |next head|------>|3|n1|------>|5|null|--_
-		 *   
-		 *                    
-		 * value = 7
-		 * head = n2
-		 * node = n3                  		                        
-		 *
-		 *
-		 * node | node.setValue | node.setNext | head
-		 *            value             head     node 
-		 *                                                   
-		 *                    		  n3          n2            n1
-		 * list-->| |next head|----> |7|n2|------>|3|n1|------>|5|null|--_
-		 *
-		 *
-		 */
 	}
 	
 	@Override
 	public String toString() {
+		StringBuffer sb = new StringBuffer();// Don't overload the memory with multiples concatennation. Used to optimize the construction of large strings
 		
-		return super.toString();
+		Node p = new Node();
+		
+		sb.append("[");
+		while(p.getNext() != null) {
+			sb.append(p.getValue() + " ");
+			p = p.getNext();
+		}
+		sb.append("]");
+		
+		return sb.toString();
 	}
+	
+	/*	p = node = n3
+	 *  p.getValue = 7
+	 *  p.getNext = n2
+	 *          
+	 *              p
+	 *       		n3                   n2            n1
+	 *  list--> |7|next head=n2|------>|3|n1|------>|5|null|--_
+	 * 
+	 * 
+	 *  p = node = n2
+	 *  p.getValue = 3
+	 *  p.getNext = n1
+	 *          
+	 *             						 p
+	 *       		n3                   n2            n1
+	 *  list--> |7|next head=n2|------>|3|n1|------>|5|null|--_
+	 * 
+	 * 
+	 * 	p = node = n1
+	 *  p.getValue = 5
+	 *  p.getNext = null
+	 *          
+	 *             									   p
+	 *       		n3                   n2            n1
+	 *  list--> |7|next head=n2|------>|3|n1|------>|5|null|--_
+	 * 
+	 * 
+	 */
 }
