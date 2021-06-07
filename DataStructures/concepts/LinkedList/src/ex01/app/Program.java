156f7eae13a8df758ea897e2f6c43aef0249c13f
gil-son <gilszon@gmail.com>
2021-06-07 16:53:58 -0300

    Menu to manage

--- DataStructures/concepts/LinkedList/src/ex01/app/Program.java
+++ DataStructures/concepts/LinkedList/src/ex01/app/Program.java
@@ -3,11 +3,14 @@ package ex01.app;
 import java.util.Locale;
 import java.util.Scanner;
 
+import ex01.util.List;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		List list = new List();
 		
 		int op;
 		
@@ -17,8 +20,11 @@ public class Program {
 			op = sc.nextInt();
 			
 			switch (op) {
-			case 1: {System.out.println("1.."); break;}
-			case 2: {System.out.println("2"); break;}
+			case 1: {System.out.println("Type the element to insert in list:"); 
+					list.add(sc.nextDouble());
+					break;}
+			
+			case 2: {System.out.println("\nThe List:\n"+list.toString()); break;}
 			case 3: {System.out.println("3"); break;}
 			default:
 				System.out.println("Invalid option: " + op);
