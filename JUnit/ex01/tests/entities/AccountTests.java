91c5300b82c63e350efd540cd82548556b7e62c4
gil-son <gilszon@gmail.com>
2021-06-10 10:39:14 -0300

    JUnit

---
+++ JUnit/ex01/tests/entities/AccountTests.java
@@ -0,0 +1,21 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+
+public class AccountTests {
+	
+	@Test
+	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+		double amount = 200.0;
+		double expectedValue = 196.0;
+		Account acc = new Account(1L, 0.0);
+		
+		acc.deposit(amount);
+		
+		// Assertions x;
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+}
