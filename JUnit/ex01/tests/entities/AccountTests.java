deb5eea969405d297438fb2aa9be586662db275d
gil-son <gilszon@gmail.com>
2021-06-13 11:35:34 -0300

    Revised methods

--- JUnit/ex01/tests/entities/AccountTests.java
+++ JUnit/ex01/tests/entities/AccountTests.java
@@ -3,7 +3,7 @@ package tests.entities;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import entities.Account;
+import entities.Account02;
 
 public class AccountTests {
 	
@@ -11,7 +11,7 @@ public class AccountTests {
 	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
 		double amount = 200.0;
 		double expectedValue = 196.0;
-		Account acc = new Account(1L, 0.0);
+		Account02 acc = new Account02(1L, 0.0);
 		
 		acc.deposit(amount);
 		
@@ -22,7 +22,7 @@ public class AccountTests {
 	@Test
 	public void depositShouldDonNothingWhenNegativeAmount() {
 		double expectedValue = 100.0;
-		Account acc  = new Account(1L, expectedValue);
+		Account02 acc  = new Account02(1L, expectedValue);
 		double amount = -200;
 		acc.deposit(amount);
 		Assertions.assertEquals(expectedValue, acc.getBalance());
@@ -31,7 +31,7 @@ public class AccountTests {
 	@Test
 	public void fullwithDrawShouldReturnZeroBalance() {
 		double expectedValue = 0.0;
-		Account acc = new Account(1L, expectedValue);
+		Account02 acc = new Account02(1L, expectedValue);
 		acc.fullWithdraw();
 		Assertions.assertEquals(expectedValue, acc.getBalance());
 	}
