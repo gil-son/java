0d80a904e6bbd93f8db7d54d066f865d90dd25d8
gil-son <gilszon@gmail.com>
2021-06-16 19:49:11 -0300

    Test with defensive programming

---
+++ JUnit/ex03/tests/entities/FinancingTests.java
@@ -0,0 +1,25 @@
+package tests.entities;
+
+import org.junit.Test;
+
+public class FinancingTests {
+
+	@Test
+	public void entryShouldByTweetyPorcentOfIncome() {
+		
+	}
+	
+	@Test
+	public void installmentShouldBeEightyPercentOfAmount() {
+		
+	}
+	
+	@Test
+	public void dontInstallmentWhenIsMoreThanHalfIncome(){
+		
+	}
+	
+	
+	
+	
+}
