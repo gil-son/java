6a1e1ac7d800698f1b7ebb70f420e4d546a679a5
gil-son <gilszon@gmail.com>
2021-06-16 20:45:09 -0300

    Factory package

---
+++ JUnit/ex03/tests/factory/FinancingFactory.java
@@ -0,0 +1,11 @@
+package tests.factory;
+
+import entities.Financing;
+
+public class FinancingFactory {
+
+	public static Financing createFinance(Double amount, Double income, Integer months) {
+		Financing f = new Financing(amount,income, months );
+		return f;
+	}
+}
