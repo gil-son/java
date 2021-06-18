42217fcead780a3341cb8f8f4e397b8fa8045da9
gil-son <gilszon@gmail.com>
2021-06-17 22:36:47 -0300

    Association Without Encapsulation

---
+++ Association/Association/src/ex01/Car.java
@@ -0,0 +1,18 @@
+package ex01;
+
+public class Car {
+	
+	 Wheel wheel;
+	 
+	 public Car() {
+		 
+	 }
+	 
+	 public Car(Wheel wheel) {
+		 this.wheel = wheel;  // this = indicate the class attribute 
+	 }
+	 
+	 
+	 
+	 
+}
---
+++ Association/Association/src/ex01/Wheel.java
@@ -0,0 +1,16 @@
+package ex01;
+
+public class Wheel {
+	
+	String texture;
+	
+	// empty constructor 
+	public Wheel() {
+		
+	}
+	
+	// constructor with parameter
+	public Wheel(String texture) {
+		this.texture = texture;
+	}
+}
