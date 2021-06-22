17894969e0792f61672e1a410e34ca327bbc0f65
gil-son <gilszon@gmail.com>
2021-06-21 21:03:18 -0300

    Class with business rules

--- JUnit/ex03/entities/Financing.java
+++ JUnit/ex03/entities/Financing.java
@@ -5,22 +5,20 @@ public class Financing {
 	private Double income;
 	private Integer months;
 	
-	public Financing() {}
+	// public Financing() {} In this case, don't use default constructor, because it will avoid Null Pointer when create an object and setter an attribute
 	
 	public Financing(Double totalAmount, Double income, Integer months) {
-		/*
-		if(totalAmount == null)
-			 throw new IllegalArgumentException("The Total Amount must be informed!"); 
-		*/
-		// 2000 >= 2000
-		if(totalAmount * 0.8 / months > income / 2.0) {
-			throw new IllegalArgumentException("The quota/installment must be less or equal than half income");
-		}
 		
+		validateFinancing(totalAmount, income, months);
 		
 		this.totalAmount = totalAmount;
 		this.income = income;
 		this.months = months;
+		/*
+		if(quota() > income / 2.0) {
+			throw new IllegalArgumentException("The quota/installment must be less or equal than half income");
+		}
+		*/
 	}
 
 	public Double getTotalAmount() {
@@ -28,10 +26,9 @@ public class Financing {
 	}
 
 	public void setTotalAmount(Double totalAmount) {
-		if(totalAmount * 0.8 / this.months > this.income / 2.0)
-			this.totalAmount = totalAmount;
-		else
-			throw new IllegalArgumentException("The new Amount is not enough. Because the half installment is more than income"); 
+		validateFinancing(totalAmount, income, months);
+		this.totalAmount = totalAmount;
+			
 	}
 
 	public Double getIncome() {
@@ -39,9 +36,7 @@ public class Financing {
 	}
 
 	public void setIncome(Double income) {
-		if(quota() > income /2.0)
-			 throw new IllegalArgumentException("The installment/quota must be half or more than income"); 
-		
+		validateFinancing(totalAmount, income, months);
 		this.income = income;	
 	}
 
@@ -50,10 +45,8 @@ public class Financing {
 	}
 
 	public void setMonths(Integer months) {
-		if(totalAmount * 0.8 / months > this.income / 2.0)
-			this.months = months;
-		else
-			throw new IllegalArgumentException("The new months is not enough. Because the half installment is more than income"); 
+		validateFinancing(totalAmount, income, months);
+		this.months = months;
 	}
 	
 	public double entry() {
@@ -63,5 +56,11 @@ public class Financing {
 	
 	public double quota() {
 		return this.totalAmount * 0.8 / months;
+	}
+	
+	public void validateFinancing(Double totalAmount, Double income, Integer months) {
+		if(totalAmount * 0.8 / months > income / 2.0) {
+			throw new IllegalArgumentException("The quota/installment must be less or equal than half income");
+		}
 	}
 }
