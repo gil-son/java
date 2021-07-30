87303bb66e0ec3b856a6ebecb5cf24825e046c04
gil-son <gilszon@gmail.com>
2021-07-30 19:36:01 -0300

    Ex01 - Lambda

---
+++ 8/Lambda/src/ex01/Java8LambdaRunnable.java
@@ -0,0 +1,29 @@
+package ex01;
+
+public class Java8LambdaRunnable {
+
+	public static void main(String[] args) {
+		
+		// Example Java 7 without Lambda
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Hello World!");
+			}
+			
+		}).run();
+		
+		
+		// Example Java 8 with Lambda
+		
+		new Thread(() -> {
+			System.out.println("Hello World!");
+		}).run();
+
+		
+				
+	}
+
+}
