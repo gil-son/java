00c5b1e23f57c5145026e30f4ceecca9082a8cfc
gil-son <gilszon@gmail.com>
2021-08-22 16:24:56 -0300

    Ex04 - Stream

--- 8/Stream/src/ex04/JavaLambdaStream.java
+++ 8/Stream/src/ex04/JavaLambdaStream.java
@@ -2,33 +2,42 @@ package ex04;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+
+
+
 
 public class JavaLambdaStream {
 	
 	public static void main(String[] args) {
 		
-		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		List<Integer> list = Arrays.asList(1,2,3,4,4,6,7,8,9);
 		
-		
+		// Are operations can be intregrantion between stream, how many is necessary
 		list.stream()
-			.skip(2)
+			.filter(e -> e % 2 == 0)
+			.skip(2) // Intermed operation
+			.skip(1) // Intermed operation
+			.limit(3)
+			.map(e -> e * 2)
+			.distinct()
 			.forEach( 
 				e -> {
 					
 					Person p = new  Person("Name: "+String.valueOf(e));
+					Car c = new  Car("Car: "+String.valueOf(e));
+					
 					System.out.println(p.getName());
-					
+					System.out.println(c.getName());
 				});
-		
 	}	
-				public class Test{
-					
+				public class Wheel{
+					private String name;
+					Wheel(){}
+					Wheel(String name){this.name = name;}
+					public String getName() {return name;}
+					public void setName(String name) {this.name = name;}
 				
 				}
-	
-	
 	
 }
 
@@ -36,9 +45,21 @@ public class JavaLambdaStream {
 
 class Car {
 	private String name;
+	private Wheel wheel;
 	Car(){}
 	Car(String name){this.name = name;}
+	Car(String name, Wheel wheel){this.name = name; this.wheel = wheel;}
 	public String getName() {return name;}
 	public void setName(String name) {this.name = name;}
 	
+}
+
+
+class Wheel{
+	private String name;
+	Wheel(){}
+	Wheel(String name){this.name = name;}
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
+
 }
