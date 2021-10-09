69e82e6d937607c851afefb5ca77bc35eb72741d
“gil-son” <gilszon@gmail.com>
2021-10-09 10:20:37 -0300

    Ex 06 - Stream

---
+++ 8/Stream/src/ex06/Java8Streams.java
@@ -0,0 +1,24 @@
+package ex06;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Comparator;
+
+public class Java8Streams {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,2,3,4,4,6,7,8,9);
+		
+		
+		// Java 8
+		
+		Optional<Integer> min = list.stream()
+		.filter( e -> e % 2 == 0)
+		.min(Comparator.naturalOrder());
+		
+		System.out.println(min.get());
+		
+	}
+}
