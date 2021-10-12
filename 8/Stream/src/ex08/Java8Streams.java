3d1a16ae6200d8fd8108efde9e1efb2e8a6ac677
“gil-son” <gilszon@gmail.com>
2021-10-12 11:06:54 -0300

    Ex 08 - Stream

---
+++ 8/Stream/src/ex08/Java8Streams.java
@@ -0,0 +1,36 @@
+package ex08;
+
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Java8Streams {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1,2,3,4,4,6,7,8,9);
+		
+		
+		// Java 8
+		
+		Map<Boolean, List<Integer>> themapBoolean = list.stream()
+		.map( e -> e * 3)
+		.collect(Collectors.groupingBy(e -> e % 2 == 0)); // The return is Boolean
+		
+		
+		System.out.println(themapBoolean);
+		
+	
+		
+		
+		Map<Integer, List<Integer>> themapInt = list.stream()
+				.map( e -> e * 3)
+				.collect(Collectors.groupingBy(e -> e % 3)); // The return is Integer
+				
+				
+				System.out.println(themapInt);
+		
+		
+	}
+}
