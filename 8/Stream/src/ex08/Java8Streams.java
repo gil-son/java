0c6345ee3c92ee29c4c9bd25d7080bbd7b1e14ea
“gil-son” <gilszon@gmail.com>
2021-10-12 11:25:30 -0300

    Ex 08 - Stream

--- 8/Stream/src/ex08/Java8Streams.java
+++ 8/Stream/src/ex08/Java8Streams.java
@@ -10,7 +10,7 @@ public class Java8Streams {
 
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(1,2,3,4,4,6,7,8,9);
-		
+		List<String> listWords = Arrays.asList("aaa", "bbb", "ccc");
 		
 		// Java 8
 		
@@ -22,15 +22,18 @@ public class Java8Streams {
 		System.out.println(themapBoolean);
 		
 	
-		
-		
 		Map<Integer, List<Integer>> themapInt = list.stream()
 				.map( e -> e * 3)
 				.collect(Collectors.groupingBy(e -> e % 3)); // The return is Integer
 				
+		System.out.println(themapInt);
 				
-				System.out.println(themapInt);
-		
+				
+		Map<Object, List<String>> themapString = listWords.stream() 
+		.collect(Collectors.groupingBy(e -> e == "aaa")); // The return is String
+				
+				
+		System.out.println(themapString);
 		
 	}
 }
