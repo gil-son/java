09bcffdf1598f04810b460d4618135bdd2a7ab2d
“gil-son” <gilszon@gmail.com>
2021-10-29 15:31:46 -0300

    Ex04 - access key and value

---
+++ Collection/Map/src/ex04/TestInterfaceMap.java
@@ -0,0 +1,26 @@
+package ex04;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestInterfaceMap {
+
+  public static void main(String[] args) {
+
+    Map<Integer, String> mapNames = new HashMap<Integer, String>();
+    
+    mapNames.put(1, "John");
+    mapNames.put(2, "Luci");
+    mapNames.put(3, "Alves");
+
+    System.out.println(mapNames);
+    
+    System.out.println(mapNames.keySet().toArray()[0]);
+    System.out.println(mapNames.get(1)); 
+
+    for( Integer key : mapNames.keySet()) {
+		System.out.println(" Key = "+key+", Value = "+mapNames.get(key));
+	}
+    
+  }
+}
