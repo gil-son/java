34af540491bd74e3325cd955ef9706a67d029d11
gilszon <gilszon@gmail.com>
2022-07-21 19:58:46 -0300

    Thread

---
+++ Thread/thread-examples/src/thread/Main.java
@@ -0,0 +1,12 @@
+package thread;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("This is a thread from main");
+
+		Thread.sleep(30000);
+	}
+
+}
